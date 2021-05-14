4fe4d8a3c3fce51991a0134dae280064f4a5f16c
Matthew Wardle <mwardle.22@gmail.com>
2021-05-14 08:07:59 -0400

    Create basic union type practice

--- course-project/basics-add.ts
+++ course-project/basics-add.ts
@@ -1,9 +1,4 @@
-const add = (
-  n1: number,
-  n2: number,
-  showResult: boolean,
-  phrase: string
-) => {
+const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
   const result = n1 + n2;
   if (showResult) {
     console.log(phrase + result);
